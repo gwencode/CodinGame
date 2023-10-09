9ae7243388a0c2eb70e82c5ed1d31c68affdf883
gwendal le bris <gwen.coding@gmail.com>
2023-10-10 01:08:41 +0200

    Do Challenge in JS

---
+++ MIME-TYPE/game.js
@@ -0,0 +1,10 @@
+let table = { "html": "text/html", "png": "image/png", "gif": "image/gif" }
+let fnames = ["animated.gif", "portrait.png", "index.html"]
+fnames.forEach((fname) => {
+  let a_fname = fname.split('.')
+  let a_length= a_fname.length - 1
+  let extension = (fname.includes('.') && a_fname[a_length] != '.' && a_fname[a_length] != undefined) ? a_fname[a_length].toLowerCase() : ""
+  // # For each of the Q filenames, display on a line the corresponding MIME type. If there is no corresponding type, then display UNKNOWN.
+  console.log(extension in table ? table[extension] : "UNKNOWN")
+  // console.log(extension)
+})
--- MIME-TYPE/game.rb
+++ MIME-TYPE/game.rb
@@ -1,19 +1,5 @@
-# Auto-generated code below aims at helping you parse
-# the standard input according to the problem statement.
-
-# n = 3 # Number of elements which make up the association table.
-# q = 3 # Number Q of file names to be analyzed.
-# table = {}
-# n.times do
-#   # ext: file extension
-#   # mt: MIME type.
-#   ext, mt = gets.split(" ")
-#   table[ext.downcase] = mt
-# end
-
 table = {"html"=>"text/html", "png"=>"image/png", "gif"=>"image/gif"}
 fnames = ["animated.gif", "portrait.png", "index.html"]
-count = 0
 fnames.each do |fname|
   extension = (fname.count('.') >= 1 && fname[-1] != '.' && fname.split('.')[-1].nil? == false) ? fname.split('.')[-1].downcase : ""
   count += 1
